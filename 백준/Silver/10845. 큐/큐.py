ecca416fce7e95af1d1760571e82da889ea083e6
uifnswi <tnwlspearl@gmail.com>
2026-04-08 14:01:29 +0900

    [Silver IV] Title: 큐, Time: 488 ms, Memory: 34936 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,27 @@
+
+from collections import deque
+dq=deque()
+n=int(input())
+for i in range(n):
+    a=input().split()
+    if a[0]=='push':
+        dq.append(a[1])
+    elif a[0]=='front':
+        if len(dq)==0:
+            print(-1)
+        else:
+            print(dq[0])
+    elif a[0]=='back':
+        if len(dq)==0:
+            print(-1)
+        else:
+            print(dq[-1])
+    elif a[0]=='size':
+        print(len(dq))
+    elif a[0]=='empty':
+        if len(dq)==0:print(1)
+        else:print(0)
+    else:
+        if len(dq)==0:
+            print(-1)
+        else:print(dq.popleft())
